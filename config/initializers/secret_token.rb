d3996d92b420cad0344f78f73abd7bae7af61ade
Murilo Machado <murilovmachado@gmail.com>
2012-12-09 22:17:46 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyMusicode::Application.config.secret_token = 'd5e76ac033a79d17eb9a985988888e2f91a290a75d9777c751ab070a967cf3a605a78a53e361af3c9b07150f2b26ced9d43511134ac9e9ce6e755a0bf73dd509'
